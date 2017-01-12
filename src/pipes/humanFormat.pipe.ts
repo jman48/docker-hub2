dded00a0bd619b91942048e627f85c9994bfe03d
John Armstrong <j.armstrong484@gmail.com>
2017-01-13 11:18:18 +1300

    Human format pipe will now accept a decimal place parameter

--- src/pipes/humanFormat.pipe.ts
+++ src/pipes/humanFormat.pipe.ts
@@ -3,8 +3,12 @@ import * as humanFormat from 'human-format';
 
 @Pipe({name: 'humanformat'})
 export class HumanFormat implements PipeTransform {
-  transform(value: number): number {
-    if (value) {
+  transform(value: number, decimals: number): number {
+    if (value && decimals) {
+      return humanFormat(value, {
+        decimals: decimals
+      });
+    } else if (value) {
       return humanFormat(value);
     }
 
